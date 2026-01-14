7432fe1d83a8da41c3fe7c633773934481ac4d18
Bella <84885135+BellaCactus@users.noreply.github.com>
2026-01-15 08:03:55 +1300

    Add files via upload

---
+++ calc/WinFormsApp1/epic calc.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(-1, -2)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(216, 23)
+        TextBox1.TabIndex = 0
+        TextBox1.Text = "click to start typing"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = SystemColors.Window
+        Label1.Location = New Point(176, 1)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 15)
+        Label1.TabIndex = 1
+        ' 
+        ' Form2
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Control
+        ClientSize = New Size(216, 20)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Cursor = Cursors.No
+        ForeColor = Color.Green
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form2"
+        Text = "epic calc(calc is short for calculator btw)"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ calc/WinFormsApp1/epic calc.vb
@@ -0,0 +1,55 @@
+﻿Public Class Form2
+
+    ' --- HELPER: Translates "Skyblock Speak" to "Math Speak" ---
+    Private Function TranslateToMath(input As String) As String
+        ' 1. Lowercase and remove spaces
+        input = input.ToLower().Replace(" ", "")
+
+        ' 2. Swap the letters for their math equivalent
+        ' k becomes *1000
+        ' m becomes *1000000
+        ' b becomes *1000000000
+        input = input.Replace("k", "*1000")
+        input = input.Replace("m", "*1000000")
+        input = input.Replace("b", "*1000000000")
+
+        Return input
+    End Function
+
+    ' --- THE REAL-TIME CALCULATOR ---
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Try
+            ' 1. Get the text from the box
+            Dim rawText As String = TextBox1.Text
+
+            ' 2. If it's empty, just reset the label
+            If rawText = "" Then
+                Label1.Text = "Waiting..."
+                Label1.ForeColor = Color.Gray
+                Exit Sub
+            End If
+
+            ' 3. Translate it (e.g. "1.5k" -> "1.5*1000")
+            Dim mathString As String = TranslateToMath(rawText)
+
+            ' 4. THE MAGIC TRICK: Use DataTable to solve the math string
+            ' This automatically handles order of operations (BODMAS) and brackets!
+            Dim dt As New DataTable()
+            Dim result As Object = dt.Compute(mathString, Nothing)
+
+            ' 5. Show the result
+            ' Convert to Decimal to format it nicely
+            Dim finalNumber As Decimal = Convert.ToDecimal(result)
+
+            ' "N0" adds the commas (e.g. 1,000,000)
+            Label1.Text = "= " & finalNumber.ToString("N0")
+            Label1.ForeColor = Color.Green
+
+        Catch ex As Exception
+            ' If the math is invalid (like "1.5m+"), just show ...
+            Label1.Text = "invalid"
+            Label1.ForeColor = Color.Red
+        End Try
+    End Sub
+
+End Class
